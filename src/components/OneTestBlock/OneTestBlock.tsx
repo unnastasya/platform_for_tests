04e3a1f62308838a2ae63277586f69b80d83ede8
Anastasiia Prokopeva <anastasiiaprokopeva31@gmail.com>
2023-07-19 20:40:19 +0500

    update OneTestBlock component

--- src/components/OneTestBlock/OneTestBlock.tsx
+++ src/components/OneTestBlock/OneTestBlock.tsx
@@ -16,21 +16,23 @@ export default function OneTestBlock({ lesson }: OneTestBlockProps) {
 	};
 
 	return (
-		<Paper onClick={linkToTest} className={styles.oneTest__block}>
-			<div className={styles.oneTest__infoBlock}>
-				<p className={styles.oneTest__lessonName}>{lesson.name}</p>
-				<div className={styles.oneTest__classesBlock}>
-					{lesson.classes.map((les: any) => (
-						<p key={les._id} className={styles.oneTest__text}>
-							{les.school},{les.class}
-						</p>
-					))}
+		<div>
+			<Paper onClick={linkToTest} className={styles.oneTest__block}>
+				<div className={styles.oneTest__infoBlock}>
+					<p className={styles.oneTest__lessonName}>{lesson.name}</p>
+					<div className={styles.oneTest__classesBlock}>
+						{lesson.classes.map((les: any) => (
+							<p key={les._id} className={styles.oneTest__text}>
+								{les.school},{les.class}
+							</p>
+						))}
+					</div>
 				</div>
-			</div>
 
-			<p className={styles.oneTest__textCount}>
-				Сдало: {lesson.doneCount} человек
-			</p>
-		</Paper>
+				<p className={styles.oneTest__textCount}>
+					Сдало: {lesson.doneCount} человек
+				</p>
+			</Paper>
+		</div>
 	);
 }
