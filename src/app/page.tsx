f15ce3950f7842f7335322a25475aa9c4e95a80c
Anastasiia Prokopeva <anastasiiaprokopeva31@gmail.com>
2023-07-19 15:15:03 +0500

    change page, layout files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+import { ThemeProvider } from "@emotion/react";
+import Link from "next/link";
+import { theme } from "../theme";
+
 export default function Home() {
-	return null;
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				<p>hello, page</p>
+				<Link href="/login">Login</Link>
+			</ThemeProvider>
+		</>
+	);
 }
