a3fd581076e575604fc6251e892fa87177a6c837
Anastasiia Prokopeva <anastasiiaprokopeva31@gmail.com>
2023-07-19 21:22:14 +0500

    add api/getClasses method

--- src/api/classes.ts
+++ src/api/classes.ts
@@ -2,9 +2,16 @@ import axios from "axios";
 
 const API_BASE_URL = "http://localhost:8080";
 
-export const getOneClass = (id: any) => {
+export const getOneClass = (id: string) => {
 	return axios
 		.get(`${API_BASE_URL}/class/${id}`)
+		.then((response) => response.data)
+		.catch((error) => error);
+};
+
+export const getClasses = () => {
+	return axios
+		.get(`${API_BASE_URL}/class`)
 		.then((response) => response.data)
 		.catch((error) => error);
 };
