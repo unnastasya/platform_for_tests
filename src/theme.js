8a18b12848817716468619fd490fd2d195724863
Anastasiia Prokopeva <anastasiiaprokopeva31@gmail.com>
2023-07-19 15:14:02 +0500

    add MUI theme file

---
+++ src/theme.js
@@ -0,0 +1,34 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+	// shadows: Array(25).fill("none"),
+	palette: {
+		primary: {
+			main: "#2979FF",
+		},
+	},
+	typography: {
+		button: {
+			textTransform: "none",
+			fontWeight: 400,
+		},
+	},
+	components: {
+		MuiTextField: {
+			styleOverrides: {
+				root: {
+					height: "50px",
+					borderRadius: "5px",
+				},
+			},
+		},
+		MuiOutlinedInput: {
+			styleOverrides: {
+				root: {
+					height: "50px",
+				},
+			},
+		},
+	},
+});
