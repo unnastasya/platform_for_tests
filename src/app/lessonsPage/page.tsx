8e5220ae4fd05b054741554ba9763fb9064a409c
Anastasiia Prokopeva <anastasiiaprokopeva31@gmail.com>
2023-07-19 17:55:01 +0500

    add lessons page

---
+++ src/app/lessonsPage/page.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { getLessons } from "@/api/lessons";
+import OneTestBlock from "@/components/OneTestBlock/OneTestBlock";
+import { Page } from "@/components/Page/Page";
+
+import styles from "./page.module.css";
+
+export default function LessonsPage() {
+	const [lessons, setLessons] = useState<any[]>([]);
+
+	useEffect(() => {
+		getLessons().then((response: any) => setLessons(response));
+	}, []);
+
+	return (
+		<>
+			<Page>
+				<div className={styles.testsPage__container}>
+					{lessons.map((lesson) => (
+						<OneTestBlock key={lesson._id} lesson={lesson} />
+					))}
+				</div>
+			</Page>
+		</>
+	);
+}
