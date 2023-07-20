ce6a383c7c439e89a4543d440e699c4d330a8ed0
Anastasiia Prokopeva <anastasiiaprokopeva31@gmail.com>
2023-07-20 12:40:41 +0500

    add DoneWorkBage component

---
+++ src/components/DoneWorkBage/DoneWorkBage.tsx
@@ -0,0 +1,60 @@
+import { Alert, Paper } from "@mui/material";
+import { useRouter } from "next/navigation";
+
+import styles from "./DoneWorkBage.module.css";
+import { DoneWorkType } from "@/types/doneWork";
+
+const whatColor = (value: number, allCriteriaRating: number) => {
+	const percentage = (value / allCriteriaRating) * 100;
+
+	if (percentage < 20) return styles.rating__question__red;
+	else if (percentage < 60) return styles.rating__question__yellow;
+	else return styles.rating__question__green;
+};
+
+interface DoneWorkBageProps {
+	work: DoneWorkType;
+}
+
+export default function DoneWorkBage({ work }: DoneWorkBageProps) {
+	const router = useRouter();
+
+	console.log(work);
+
+	const toOneWorkPage = (id: string, workIsVerified: boolean) => {
+		if (workIsVerified) {
+			router.push(`/resultTest/${id}`);
+		} else {
+			router.push(`/checkTestPage/${id}`);
+		}
+	};
+
+	return (
+		<div
+			key={work._id}
+			onClick={() => toOneWorkPage(work._id, work.isVerified)}
+			className={styles.clickable}
+		>
+			<Paper sx={{ margin: 0 }} className={styles.student__work}>
+				<p className={styles.student__textHeader}>Урок 21.03.2022</p>
+				<p className={styles.student__text}>Описание урока</p>
+				{work.rating && (
+					<p
+						className={`${styles.rating__question} ${whatColor(
+							work.rating,
+							work.allCriteriaRating
+						)}`}
+					>
+						{work.rating}
+					</p>
+				)}
+
+				{work.isVerified ? (
+					<Alert severity="success">Проверено</Alert>
+				) : (
+					<Alert severity="warning">Ожидает проверки</Alert>
+				)}
+			</Paper>
+		</div>
+	);
+}
