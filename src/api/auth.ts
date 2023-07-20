0062d5aa366010a225b513e9840a4d8443bc698c
Anastasiia Prokopeva <anastasiiaprokopeva31@gmail.com>
2023-07-20 11:34:35 +0500

    add getUsersByClassId method

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -11,3 +11,9 @@ export const login = (data: any) => {
 			errorMessage: error.response.data.error,
 		}));
 };
+
+export const getUsersByClassId = (id: string) => {
+	return axios
+		.get(`${API_BASE_URL}/userByClassId/${id}`)
+		.then((response) => response.data);
+};
