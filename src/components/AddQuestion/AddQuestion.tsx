d1d55c2092f7654f93039180a78c22185df3f08d
Anastasiia Prokopeva <anastasiiaprokopeva31@gmail.com>
2023-07-20 16:17:26 +0500

    add AddQuestion component

---
+++ src/components/AddQuestion/AddQuestion.tsx
@@ -0,0 +1,99 @@
+import { useFieldArray } from "react-hook-form";
+import { Button, FormControl, Paper, TextField } from "@mui/material";
+import { AddCriteria } from "../AddCriteria/AddCriteria";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+import styles from "./AddQuestion.module.css";
+
+export default function AddQuestion({ control, register }: any) {
+	const { fields, append, remove } = useFieldArray({
+		name: "questions",
+		control,
+		rules: {
+			required: "Пожалуйста, добавьте хотя бы один вопрос",
+		},
+	});
+
+	return (
+		<div>
+			{fields.map((field, index) => {
+				return (
+					<Paper
+						key={index}
+						className={styles.addQuestion__container}
+					>
+						<div className={styles.addQuestion__header}>
+							<p className={styles.addQuestion__textNumber}>
+								Вопрос {index + 1}
+							</p>
+							<Button
+								variant="contained"
+								aria-label="delete"
+								onClick={() => {
+									remove(index);
+								}}
+							>
+								<DeleteIcon
+									sx={{ height: "45px", width: "30px" }}
+								/>
+							</Button>
+						</div>
+						<div className={styles.addQuestion__nameBlock}>
+							<TextField
+								{...register(`questions.${index}.questionText`)}
+								fullWidth
+								multiline
+								label="Задание"
+							/>
+							<TextField
+								{...register(`questions.${index}.description`)}
+								fullWidth
+								multiline
+								label="Описание задания"
+							/>
+							<FormControl fullWidth>
+								<TextField
+									sx={{ marginBottom: "20px" }}
+									id="outlined-multiline-static"
+									multiline
+									label="Поле для ввода ответа"
+									disabled
+								/>
+							</FormControl>
+						</div>
+
+						<hr
+							style={{
+								width: "100%",
+								border: "none",
+								backgroundColor: "#A3A3A3",
+								color: "#A3A3A3",
+								height: "1px",
+							}}
+						/>
+
+						<AddCriteria
+							indexQuestion={index}
+							register={register}
+							control={control}
+						/>
+					</Paper>
+				);
+			})}
+			<Button
+				onClick={() => {
+					append({
+						id: Number(Date.now()),
+						question: "",
+						description: "",
+						criteria: [{ text: "", count: 0 }],
+						criteriaRating: 0,
+					});
+				}}
+				variant="outlined"
+			>
+				Еще вопрос
+			</Button>
+		</div>
+	);
+}
