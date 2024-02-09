8e470e295cd5b4946f388ff22233aea60d4f1f0a
unnastasya <unnastasya@gmail.com>
2024-02-09 19:18:52 +0500

    fix shadow paper and alert components

--- src/theme.js
+++ src/theme.js
@@ -58,8 +58,16 @@ export const theme = createTheme({
 			styleOverrides: {
 				root: {
 					borderRadius: "10px",
+					boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)",
 				},
 			},
 		},
+        MuiAlert: {
+            styleOverrides: {
+                root: {
+                    boxShadow: "none"
+                }
+            }
+        }
 	},
 });
